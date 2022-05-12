737aa639f11e185938b601c84410dc022002f033
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-05-12 21:12:08 +0300

    completing task (except filter)

---
+++ src/main/java/main/CalculatorServlet.java
@@ -0,0 +1,143 @@
+package main;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+
+
+@WebServlet(urlPatterns = {"/calc/*"})
+public class CalculatorServlet extends HttpServlet implements CalcInterface {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        String requestArgument = new StringBuilder(request.getPathInfo()).substring(1);
+
+        if (session.getAttribute(requestArgument) == null) {
+            response.setStatus(SC_CREATED);
+        }
+
+        if (requestArgument.equals(EXPRESSION_TEXT)) {
+            checkingExpression(request, response, requestArgument);
+        } else {
+            checkingValueSize(request, response, requestArgument);
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        String variableForDelete = new StringBuilder(request.getPathInfo()).substring(1);
+        session.setAttribute(variableForDelete, null);
+        response.setStatus(SC_NO_CONTENT);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        Calculator calculator = new Calculator();
+        EquationCreator equation = new EquationCreator();
+        try {
+            List<String> tokens = equation.sortEquationElements(session.getAttribute(EXPRESSION_TEXT).toString());
+            Map<String, String> childVariableMap = new HashMap<>();
+            Enumeration<String> allNamesAttributes = session.getAttributeNames();
+            Map<String, Integer> argumentsMap = attributeParse(session, allNamesAttributes, childVariableMap);
+            for (Map.Entry<String, String> keyVariable : childVariableMap.entrySet()) {
+                Integer valueVariable = argumentsMap.get(childVariableMap.get(keyVariable.getKey()));
+                argumentsMap.put(keyVariable.getKey(), valueVariable);
+            }
+
+            int result = calculator.calculate(tokens, argumentsMap);
+            session.setAttribute(RESULT_TEXT, result);
+        } catch (NumberFormatException exception) {
+            exception.printStackTrace();
+            response.setStatus(SC_CONFLICT);
+        }
+
+        try {
+            response.getWriter().print(session.getAttribute(RESULT_TEXT));
+        } catch (IOException exception) {
+            LOGGER.log(Level.WARNING, exception.getMessage());
+        }
+    }
+
+    private void checkingExpression(HttpServletRequest request, HttpServletResponse response, String keyInfo) {
+        String valueData = readALineOfText(request);
+        if (valueData.chars().mapToObj(i->(char)i).noneMatch(i -> OPERATIONS_LIST.contains(String.valueOf(i)))) {
+            response.setStatus(SC_BAD_REQUEST);
+            return;
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute(keyInfo, valueData);
+    }
+
+    private void checkingValueSize(HttpServletRequest request, HttpServletResponse response, String keyInfo) {
+        HttpSession session = request.getSession();
+        String valueData = EMPTY_STRING;
+        try {
+            valueData = readALineOfText(request);
+            int number = Integer.parseInt(valueData);
+            if (number < MIN_VALUES_RANGE || number > MAX_VALUES_RANGE) {
+                response.setStatus(SC_FORBIDDEN);
+                return;
+            }
+            session.setAttribute(keyInfo, valueData);
+        } catch (NumberFormatException exception) {
+            exception.printStackTrace();
+            session.setAttribute(keyInfo, valueData);
+            response.setStatus(SC_BAD_REQUEST);
+        }
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+
+    }
+
+    private Map<String, Integer> attributeParse(HttpSession session, Enumeration<String> allNamesAttributes,
+                                Map<String, String> childVariableMap) {
+        Map<String, Integer> argumentMap = new HashMap<>();
+        while (allNamesAttributes.hasMoreElements()) {
+            String attributeName = allNamesAttributes.nextElement();
+            if (attributeName.length() != 1) {
+                continue;
+            }
+            putAttributesInArgs(session, argumentMap, childVariableMap, attributeName);
+        }
+        return argumentMap;
+    }
+
+    private void putAttributesInArgs(HttpSession session, Map<String, Integer> args,
+                                     Map<String, String> variablesHavingNameOfAnotherVariable, String attributeName) {
+        String valueOfAttribute = session.getAttribute(attributeName).toString();
+        try {
+            args.put(attributeName, Integer.parseInt(valueOfAttribute));
+        } catch (NumberFormatException e) {
+            variablesHavingNameOfAnotherVariable.put(attributeName, valueOfAttribute);
+        }
+    }
+
+    private String readALineOfText(ServletRequest request) {
+        String valueData = EMPTY_STRING;
+        try {
+            valueData = request.getReader().readLine();
+        } catch (IOException exception) {
+            LOGGER.log(Level.WARNING, exception.getMessage());
+        }
+        return valueData;
+    }
+
+
+
+
+}
---
+++ src/main/java/main/EquationCreator.java
@@ -0,0 +1,82 @@
+package main;
+
+import java.util.*;
+
+public class EquationCreator implements CalcInterface {
+
+    public synchronized List<String> sortEquationElements(String equation) {
+        List<String> tokensList = new ArrayList<>();
+        Deque<String> symobolsStack = new ArrayDeque<>();
+        List<String> equationElementsNotation = new ArrayList<>();
+
+        StringTokenizer tokenizer = new StringTokenizer(deleteWhitespaceInEquation(equation),
+                OPERATIONS_AND_PARENTHESIS, true);
+        while (tokenizer.hasMoreElements()) {
+            tokensList.add(tokenizer.nextToken());
+        }
+        addEquationTokens(tokensList, symobolsStack, equationElementsNotation);
+        addRemainingTokens(symobolsStack, equationElementsNotation);
+        return equationElementsNotation;
+    }
+
+    private void addEquationTokens(List<String> tokens, Deque<String> stackSymbols,
+                                   List<String> elementsList) {
+        for (String token : tokens) {
+            try {
+                switch (token) {
+                    case PLUS:
+                    case SUBTRACT:
+                    case CLOSING_PARENTHESIS:
+                        addAllSymbols(stackSymbols, elementsList, token);
+                        break;
+                    case MULTIPLY:
+                    case DIVIDE:
+                        addMultiplicationAndDivisionSymbols(stackSymbols, elementsList, token);
+                        break;
+                    case OPENING_PARENTHESIS:
+                        stackSymbols.addFirst(token);
+                        break;
+                    default:
+                        elementsList.add(token);
+                }
+            } catch (NoSuchElementException exception) {
+                LOGGER.info(exception.getMessage());
+                if (!token.equals(CLOSING_PARENTHESIS)) {
+                    stackSymbols.addFirst(token);
+                }
+            }
+        }
+    }
+
+    private void addRemainingTokens(Deque<String> symbolsStack, List<String> equationElementsNotation) {
+        while (!symbolsStack.isEmpty()) {
+            if (symbolsStack.getFirst().equals(OPENING_PARENTHESIS)) {
+                return;
+            }
+            equationElementsNotation.add(symbolsStack.removeFirst());
+        }
+    }
+
+    private String deleteWhitespaceInEquation(String equation) {
+        return equation.replace(WHITESPACE_SYMBOL, EMPTY_STRING);
+    }
+
+    private void addAllSymbols(Deque<String> stackSymbols, List<String> elementsList, String token) {
+        while (OPERATIONS_LIST.contains(stackSymbols.getFirst())) {
+            elementsList.add(stackSymbols.removeFirst());
+        }
+        if (token.equals(CLOSING_PARENTHESIS)) {
+            stackSymbols.removeFirst();
+        } else {
+            stackSymbols.addFirst(token);
+        }
+    }
+
+    private void addMultiplicationAndDivisionSymbols(Deque<String> stackSymbols, List<String> equationElementsNotation,
+                                                     String token) {
+        while (MULTIPLY_AND_DIVIDE.contains(stackSymbols.getFirst())) {
+            equationElementsNotation.add(stackSymbols.removeFirst());
+        }
+        stackSymbols.addFirst(token);
+    }
+}
